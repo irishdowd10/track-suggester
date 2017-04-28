8e80fb2bd724ec299718b5f269f04799502798c9
John Dowd <john.dowd@inspectall.com>
2017-04-28 13:36:28 -0700

    chage logic for js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,42 @@
 $(document).ready(function() {
-  $(".formOne").submit(function(event) {
-  var nameInput = $("input#name").val();
-  var questionOne = parseInt($("select#questionOne").val());
-  var questionTwo = parseInt($("select#questionTwo").val());
-  var questionThree =  parseInt($("select#questionThree").val());
-  var questionFour = parseInt($("select#questionFour").val());
-  var questionFive =  parseInt($("select#questionFive").val());
-  var result = questionOne + questionTwo + questionThree + questionFour + questionFive;
-
-  if (result<= 5) {
-    $(".option").hide();
-    $("#css").show();
-    }
-   else if (result<=7) {
-     $(".option").hide();
-      $("#ruby").show();
-    }
-    else if (result<=8){
-      $(".option").hide();
-      $("#c").show();
-    }
-
-  $(".name").text(nameInput);
+  $("#questions").submit(function(event) {
     event.preventDefault();
 
-  });
+    // var arrayOfQuestions = ['#questionOne', '#questionTwo', '#questionThree',
+    //                         '#questionFour', '#questionFive'];
 
+    var nameInput = $("input#name").val();
+
+    // var result = 0;
+
+    // arrayOfQuestions.length = 5;
+
+    // for(var x = 0; x < arrayOfQuestions.length; x++) {
+    //   result += parseInt($(arrayOfQuestions[x]).val());
+    // }
+    var questionOne = parseInt($("#questionOne").val());
+    var questionTwo = parseInt($("#questionTwo").val());
+    var questionThree =  parseInt($("#questionThree").val());
+    var questionFour = parseInt($("#questionFour").val());
+    var questionFive =  parseInt($("#questionFive").val());
+    result = questionOne + questionTwo + questionThree + questionFour + questionFive;
+
+
+    if(!isNaN(result)) {
+      if (result <= 5) {
+        $(".option").hide();
+        $("#css").show();
+      } else if (result <= 6) {
+        $(".option").hide();
+        $("#ruby").show();
+      } else if (result <= 8) {
+        $(".option").hide();
+        $("#c").show();
+      }
+    } else {
+      window.alert('Please select all values!')
+    }
+
+    $(".name").text(nameInput);
   });
+});
